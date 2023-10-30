b958a7d7e2ef2d805faaa040a6fef9c8ef6af193
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-10-30 20:38:27 +0500

    Additions

--- src/Pages/Components/Product.js
+++ src/Pages/Components/Product.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsPlus, BsEye } from "react-icons/bs";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div className="flex items-center justify-center w-full h-full ">
+          <div className="w-[200px] mx-auto flex justify-center items-center">
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  );
 };
 
 export default Product;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { ProductContext } from "../ProductContext";
+import Product from "./Components/Product";
 
 const Home = () => {
   const { products } = useContext(ProductContext);
@@ -9,7 +10,7 @@ const Home = () => {
       item.category === "men's clothing" || item.category === "women's clothing"
     );
   });
-  console.log(filteredProducts);
+  // console.log(filteredProducts);
 
   return (
     <div>
@@ -17,11 +18,7 @@ const Home = () => {
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts.map((product) => {
-              return (
-                <div className="w-full h-[300px] bg-pink-200" key={product.id}>
-                  {product.title}{" "}
-                </div>
-              );
+              return <Product product={product} key={product.id} />;
             })}
           </div>
         </div>
