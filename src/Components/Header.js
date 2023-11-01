e6f007d1950aa91dc3ff8841d4fdd786f4e741f0
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-11-01 16:45:25 +0500

    Side bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,18 @@
 import React, { useContext } from "react";
 import { SidebarContext } from "../Context/SidebarContext";
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
   const { isOpen, setIsOpen } = useContext(SidebarContext);
   return (
     <div>
       <div>Header</div>
-      <div onClick={() => setIsOpen(!isOpen)}>Open/Close</div>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="relative flex cursor-pointer"
+      >
+        <BsBag className="text-2xl" />
+      </div>
     </div>
   );
 };
--- src/Pages/SideBar.js
+++ src/Pages/SideBar.js
@@ -6,14 +6,22 @@ import CartItem from "../Components/CartItem";
 import { SidebarContext } from "../Context/SidebarContext";
 
 const SideBar = () => {
-  const { isOpen, handleOpen } = useContext(SidebarContext);
+  const { isOpen, handleClose } = useContext(SidebarContext);
   return (
     <div
       className={`${
         isOpen ? "right-0" : "-right-full"
       } fixed top-0 w-full h-full bg-white shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
-      SideBar
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="text-sm font-semibold uppercase">Shopping Bag (0)</div>
+        <div
+          className="flex items-center justify-center w-8 h-8 cursor-pointer"
+          onClick={handleClose}
+        >
+          <IoMdArrowForward className="text-2xl" />
+        </div>
+      </div>
     </div>
   );
 };
